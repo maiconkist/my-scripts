1d0423aa2766753b228183ee75cbdf9d863785f6
nodeuser <nodeuser@localhost.localdomain>
2017-03-28 16:56:43 +0100

    update

--- hydra/img_from_bin_file.py
+++ hydra/img_from_bin_file.py
@@ -11,23 +11,12 @@ def from_bin_file(fname):
 
     return arr
 
-def gen_img(arr, perform_fft, fft_size, fname, nfft_to_group = 1):
+def gen_img(arr, perform_fft, fft_size, fname, nfft_to_group):
         # cut off samples in the end of the file that do not fit a FFT
         arr = np.resize(arr, arr.size - arr.size % fft_size)
 
+        print '\t ... reshaping'
         arr = arr.reshape(arr.size/fft_size, fft_size)
-
-        # reshape
-        if nfft_to_group > 1:
-            print '\t ... reshaping'
-
-            arr2 = []
-            for i in range(1, arr.size / (fft_size*nfft_to_group)):
-                arr2.append([arr[j] for j in range(i, i + nfft_to_group)])
-            arr = np.array(arr2)
-
-            print '\t ... averaging'
-            arr = np.mean(arr, axis=1)
 
         if perform_fft:
             print '\t ... fft'
@@ -36,15 +25,41 @@ def gen_img(arr, perform_fft, fft_size, fname, nfft_to_group = 1):
         print '\t ... complex to mag'
         arr = complex_to_mag(arr)
 
+        # reshape
+        if nfft_to_group > 1:
+            print '\t ... grouping'
+            arr2 = []
+            for i in range(0, len(arr)/nfft_to_group):
+                gsum = np.array([0.0 for _tmp in range(fft_size)])
+                print str(i) + "/" + str(len(arr))
+                gsum = arr[0:min(nfft_to_group, len(arr)-1)]
+                arr = np.delete(arr, range(0,min(nfft_to_group, len(arr)-1)), 0)
+                arr2.append(gsum.sum(axis=0)/len(gsum))
+
+                print arr2[-1]
+
+            arr = np.array(arr2)
+
+        print '\t ... normalizing'
+        arr = arr - np.min(arr)
+        arr = arr / np.max(arr)
+        print '\t\t ... avg: ' + str(np.average(arr))
+
+        print '\t\t ... binarizing: '
+        low = arr <= (np.average(arr) + 3 * np.std(arr))
+        high = arr > (np.average(arr) + 3 * np.std(arr))
+        arr[low] = 0
+        arr[high] = 1
+
         print '\t ... generating heatmap'
-        plt.imshow(arr, cmap='hot')
+        plt.imshow(arr, cmap='gist_gray')
 
         print '\t ... saving'
         plt.savefig(fname)
         print '\t ... done'
 
 def complex_to_mag(ar):
-    return 20 * np.log10(np.absolute(ar))
+    return np.log10(np.absolute(ar))
 
 def main(options):
 
@@ -52,23 +67,18 @@ def main(options):
 
     fname = lambda idx: options.in_file + str(idx) + "." + options.out_file_ext
 
-    nfft_to_group = 1
-    if options.avg_by_time > 0:
-         nfft_to_group = int((options.samp_rate/options.fft_size) * (options.avg_by_time/1000.0))
-         print 'FFTs to group: %d' % (nfft_to_group, )
-
     inpos = options.in_pos
     if options.lines_per_file == -1:
         print "Generating a single image. This can take a while ..."
         block_size = len(arr)
     else:
-        block_size = options.lines_per_file * options.fft_size * nfft_to_group
+        block_size = options.lines_per_file * options.fft_size * options.nfft_to_group
 
     if options.out_pos == -1:
         options.out_pos = len(arr)
 
     while inpos < options.out_pos:
-        gen_img(arr[inpos:inpos+block_size], options.perform_fft, options.fft_size, fname(inpos/block_size), nfft_to_group)
+        gen_img(arr[inpos:inpos+block_size], options.perform_fft, options.fft_size, fname(inpos/block_size), options.nfft_to_group)
 
         inpos += block_size
 
@@ -90,8 +100,8 @@ if __name__ == "__main__":
                      help="Ouput image extension.")
     parser.add_option("", "--samp-rate", type="float", default=1e6,
                       help="Sample rate used to generate file. Used to group FFT bins by TIME.")
-    parser.add_option("", "--avg-by-time", type="int", default=-1,
-                      help="In milliseconds. Group FFT BINS by time based on the SAMP RATE.")
+    parser.add_option("", "--nfft-to-group", type="int", default=1,
+                      help="Group block of FFTs and extract the average.")
     (options, args) = parser.parse_args ()
 
     main(options)
