23b8296b696a6e880beca2efc9a49b5dc79065b1
Maicon Kist <maiconkist@gmail.com>
2017-03-30 15:06:43 +0100

    updates

--- img_from_bin_file.py
+++ img_from_bin_file.py
@@ -4,8 +4,19 @@ import sys
 from optparse import OptionParser
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 from PIL import Image
+
+
+hydra_filename_template= 'hydra/{mode}_{A1}A1_{A2}A2_4Msps_20Ms.bin'
+mode = ['atomic', ]
+A1 = ["00", "01", "05", "1"] # amplitudes for VR 1
+A2 = ["00", "01", "05", "1"] # amplitudes for VR 2
+
+single_filename_template= 'single/{radio}_{A}_4Msps_20Ms.bin'
+radio = ['lte', 'nb_iot']
+A = ["00", "01", "05", "1"] # amplitudes for VR 1
 
 def from_bin_file(fname):
     fd = open(fname, "rb")
@@ -47,10 +58,12 @@ def gen_img(arr, options, fname):
             arr = np.array(arr2)
 
         print '\t ... normalizing'
-        arr = arr - np.min(arr)
-        arr = arr / np.max(arr)
+        print '\t\t ... min: ' + str(np.min(arr))
+        print '\t\t ... max: ' + str(np.max(arr))
         print '\t\t ... avg: ' + str(np.average(arr))
         print '\t\t ... std: ' + str(np.std(arr))
+        arr = arr - np.min(arr)
+        arr = arr / np.max(arr)
 
         print '\t ... saving colored imag'
         img = Image.fromarray(np.uint8(plt.get_cmap('Accent')(arr) * 255.0))
@@ -70,7 +83,7 @@ def gen_img(arr, options, fname):
 def complex_to_mag(ar):
     return np.log10(np.absolute(ar))
 
-def main(options):
+def parse_file(options):
 
     arr = from_bin_file(options.in_file)
 
@@ -92,28 +105,53 @@ def main(options):
 
         inpos += block_size
 
+
+def parse_all_files(options):
+
+    for _r, _d, _f in os.walk(options.folder):
+        if '.git' in _r:
+            continue
+
+        bin_files = [b for b in _f if b.endswith('.bin')]
+
+        for bf in bin_files:
+            options.in_file = _r + '/' + bf
+            print 'Parsing file: ' + options.in_file
+
+            parse_file(options)
+
+
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-f", "--in-file", type="string", default="./signal.bin",
-                     help="Bin file to read.")
+                     help="Bin file to read [default=%default].")
     parser.add_option("", "--fft-size", type="int", default=256,
-                     help="FFT size used/to use.")
+                     help="FFT size used/to use [default=%default].")
     parser.add_option("", "--perform-fft", action="store_true", default=False,
-                     help="Perform FFT.")
+                     help="Perform FFT [default=%default].")
     parser.add_option("", "--lines-per-file", type="int", default=-1,
-                     help="Block of samples to read from FILE.")
+                     help="Block of samples to read from FILE [default=%default].")
     parser.add_option("", "--in-pos", type="int", default=0,
-                     help="Index of first sample to read.")
+                     help="Index of first sample to read [default=%default].")
     parser.add_option("", "--out-pos", type="int", default=-1,
-                     help="Index of last sample to read.")
+                     help="Index of last sample to read [default=%default].")
     parser.add_option("", "--out-file-ext", type="string", default="png",
-                     help="Ouput image extension.")
+                     help="Ouput image extension [default=%default].")
     parser.add_option("", "--samp-rate", type="float", default=1e6,
-                      help="Sample rate used to generate file. Used to group FFT bins by TIME.")
+                      help="Sample rate used to generate file. Used to group FFT bins by TIME [default=%default].")
     parser.add_option("", "--nfft-to-group", type="int", default=1,
-                      help="Group block of FFTs and extract the average.")
+                      help="Group block of FFTs and extract the average [default=%default].")
     parser.add_option("", "--binarize", action="store_true", default=False,
-                     help="Save binarized version of image.")
+                     help="Save binarized version of image [default=%default].")
+
+    batch_group = parser.add_option_group('Batch Converstion')
+    batch_group.add_option("", "--folder", type="string", default='.',
+                     help="Specify the folder to parse all files [default=%default].")
+    batch_group.add_option("", "--parse-all", action="store_true", default=False,
+                     help="Parse all files in FOLDER [default=%default].")
     (options, args) = parser.parse_args ()
 
-    main(options)
+    if options.parse_all:
+        parse_all_files(options)
+    else:
+        parse_file(options)
