df66ef0c2b9ab8ee41acbb9c8693b4bb6297b62e
Maicon Kist <maiconkist@gmail.com>
2017-10-02 18:22:35 +0100

    updates

--- mse/auto_mse.bash
+++ mse/auto_mse.bash
@@ -1,17 +1,33 @@
 FFT=(512 1024 2048 4096)
 
-for fft in ${FFT[@]}; do
+#for fft in ${FFT[@]}; do
+#
+#    vr1_mid_fft="$(python -c "print((${fft}/2)+((947.5e6-950e6) / (6e6/${fft})))")"
+#    vr1_fi_fft="$(python -c "print(int(${vr1_mid_fft} - (1e6/2/(6e6/${fft}))))")"
+#    vr1_la_fft="$(python -c "print(${vr1_mid_fft} + (1e6/2/(6e6/${fft})))")"
+#    vr1_fft="$(python -c "print(int(${vr1_la_fft} - ${vr1_fi_fft}))")"
+#
+#    vr2_mid_fft="$(python -c "print((${fft}/2)+((951e6-950e6) / (6e6/${fft})))")"
+#    vr2_fi_fft="$(python -c "print(int(${vr2_mid_fft} - (500e3/2/(6e6/${fft}))))")"
+#    vr2_la_fft="$(python -c "print(${vr2_mid_fft} + (500e3/2/(6e6/${fft})))")"
+#    vr2_fft="$(python -c "print(int(${vr2_la_fft} - ${vr2_fi_fft}))")"
+#
+#	 for it in `seq 0 10`; do
+#		  python hydra_dump_to_files.py --fftm ${fft} --filepattern "./${fft}-${it}" --vr1fft ${vr1_fft} --vr1offset ${vr1_fi_fft} --vr2fft ${vr2_fft} --vr2offset ${vr2_fi_fft}
+#    done
+#done
 
-    vr1_mid_fft="$(python -c "print((${fft}/2)+((947.5e6-950e6) / (6e6/${fft})))")"
-    vr1_fi_fft="$(python -c "print(int(${vr1_mid_fft} - (1e6/2/(6e6/${fft}))))")"
-    vr1_la_fft="$(python -c "print(${vr1_mid_fft} + (1e6/2/(6e6/${fft})))")"
-    vr1_fft="$(python -c "print(int(${vr1_la_fft} - ${vr1_fi_fft}))")"
+python ../parse_bins.py --gen-mse --src-folder ./ --dst-folder ./ --mse-file mse.csv
 
-    vr2_mid_fft="$(python -c "print((${fft}/2)+((951e6-950e6) / (6e6/${fft})))")"
-    vr2_fi_fft="$(python -c "print(int(${vr2_mid_fft} - (500e3/2/(6e6/${fft}))))")"
-    vr2_la_fft="$(python -c "print(${vr2_mid_fft} + (500e3/2/(6e6/${fft})))")"
-    vr2_fft="$(python -c "print(int(${vr2_la_fft} - ${vr2_fi_fft}))")"
-
-    python hydra_dump_to_files.py --fftm ${fft} --filepattern "./${fft}" --vr1fft ${vr1_fft} --vr1offset ${vr1_fi_fft} --vr2fft ${vr2_fft} --vr2offset ${vr2_fi_fft}
-
-done
+awk '{
+      mse[$2][$1] += $4;
+      mseq[$2][$1] += ($4 ^ 2);
+      count[$2][$1] += 1;
+    }
+    END{
+      for (vr in mse) {
+		    for (fft in mse[vr]){
+           print fft " " vr " " mse[vr][fft]/count[vr][fft] " " sqrt((mseq[vr][fft]-mse[vr][fft]^2/count[vr][fft])/count[vr][fft]);
+      }
+		}
+    }' mse.csv > plottable.csv
--- mse/hydra_dump_to_files.py
+++ mse/hydra_dump_to_files.py
@@ -4,7 +4,7 @@
 # GNU Radio Python Flow Graph
 # Title: OFDM Tx
 # Description: Example of an OFDM Transmitter
-# Generated: Mon Oct  2 12:27:45 2017
+# Generated: Mon Oct  2 17:47:41 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -27,6 +27,7 @@ from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.fft import window
 from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
 import hydra
 import numpy
@@ -37,7 +38,7 @@ from gnuradio import qtgui
 
 class hydra_dump_to_files(gr.top_block, Qt.QWidget):
 
-    def __init__(self, fftm=512, filepattern="./", freq=950e6, freq1=947.5e6, freq2=951e6, samprate=6e6, samprate1=1e6, samprate2=500e3, vr1fft=170, vr1offset=0, vr2fft=85, vr2offset=0):
+    def __init__(self, fftm=128, filepattern="./", freq=950e6, freq1=947.5e6, freq2=951e6, samprate=6e6, samprate1=1e6, samprate2=500e3, vr1fft=512, vr1offset=0, vr2fft=512, vr2offset=319):
         gr.top_block.__init__(self, "OFDM Tx")
         Qt.QWidget.__init__(self)
         self.setWindowTitle("OFDM Tx")
@@ -90,10 +91,18 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
         self.packet_len = packet_len = 100
         self.occupied_carriers = occupied_carriers = (sorted(tuple(set([x for x in range(-26,27)]) - set(pilot_carriers[0]) - set([0,]))),)
         self.length_tag_key = length_tag_key = "packet_len"
+        self.amplitude2 = amplitude2 = 1.0
+        self.amplitude1 = amplitude1 = 1.0
 
         ##################################################
         # Blocks
         ##################################################
+        self._amplitude2_range = Range(0, 1.0, 0.01, 1.0, 200)
+        self._amplitude2_win = RangeWidget(self._amplitude2_range, self.set_amplitude2, 'amplitude2', "counter_slider", float)
+        self.top_layout.addWidget(self._amplitude2_win)
+        self._amplitude1_range = Range(0.0, 1.0, 0.01, 1.0, 200)
+        self._amplitude1_win = RangeWidget(self._amplitude1_range, self.set_amplitude1, 'amplitude1', "counter_slider", float)
+        self.top_layout.addWidget(self._amplitude1_win)
         self.qtgui_freq_sink_x_0_0_0 = qtgui.freq_sink_c(
         	1024, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -180,62 +189,14 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-        	1024, #size
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	0, #fc
-        	1e6, #bw
-        	"", #name
-        	1 #number of inputs
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(False)
-
-        if not True:
-          self.qtgui_freq_sink_x_0.disable_legend()
-
-        if "complex" == "float" or "complex" == "msg_float":
-          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.hydra_hydra_sink_0 = hydra.hydra_sink(2, fftm, freq, samprate,
-        	 ((freq1, samprate1),
-        	 (freq2, samprate2),
+        self.hydra_hydra_sink_0 = hydra.hydra_sink(1, fftm, freq, samprate,
+        	 ((freq, samprate),
         	 ))
 
-        self.fft_vxx_0_1 = fft.fft_vcc(fftm, True, (window.blackmanharris(fftm)), True, 1)
-        self.fft_vxx_0_0_0 = fft.fft_vcc(vr2fft, False, (window.rectangular(vr2fft)), True, 1)
-        self.fft_vxx_0_0 = fft.fft_vcc(vr1fft, False, (window.rectangular(vr1fft)), True, 1)
-        self.fft_vxx_0 = fft.fft_vcc(fftm, True, (window.blackmanharris(fftm)), True, 1)
         self.digital_ofdm_tx_0_1 = digital.ofdm_tx(
         	  fft_len=64, cp_len=16,
         	  packet_length_tag_key=length_tag_key,
-        	  occupied_carriers=occupied_carriers,
+        	  occupied_carriers=(sorted(tuple(set([x for x in range(-15,15)]) - set(pilot_carriers[0]) - set([0,]))),),
         	  pilot_carriers=pilot_carriers,
         	  pilot_symbols=pilot_symbols,
         	  sync_word1=[0., 0., 0., 0., 0., 0.,] + pattern1 * ((64-12)/len(pattern1))  +[0., 0., 0., 0., 0., 0.,] ,
@@ -249,7 +210,7 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
         self.digital_ofdm_tx_0 = digital.ofdm_tx(
         	  fft_len=128, cp_len=16,
         	  packet_length_tag_key=length_tag_key,
-        	  occupied_carriers=occupied_carriers,
+        	  occupied_carriers=(sorted(tuple(set([x for x in range(-60,60)]) - set(pilot_carriers[0]) - set([0,]))),),
         	  pilot_carriers=pilot_carriers,
         	  pilot_symbols=pilot_symbols,
         	  sync_word1=sync_word1,
@@ -260,24 +221,14 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
         	  debug_log=False,
         	  scramble_bits=False
         	 )
-        self.blocks_vector_to_stream_0_1 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, fftm)
-        self.blocks_vector_to_stream_0_0_0 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, vr2fft)
-        self.blocks_vector_to_stream_0_0 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, vr1fft)
-        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, fftm)
+        self.blocks_throttle_1 = blocks.throttle(gr.sizeof_gr_complex*1, 6e6,True)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, 6e6,True)
-        self.blocks_stream_to_vector_0_1 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, fftm)
-        self.blocks_stream_to_vector_0_0_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, vr2fft)
-        self.blocks_stream_to_vector_0_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, vr1fft)
-        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, fftm)
         self.blocks_stream_to_tagged_stream_0_1 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, packet_len/2, length_tag_key)
         self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, packet_len, length_tag_key)
-        self.blocks_multiply_const_vxx_1_0 = blocks.multiply_const_vcc((1.0/vr2fft, ))
-        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vcc((1.0/vr1fft, ))
-        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vcc((0.1, ))
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.1, ))
-        self.blocks_keep_m_in_n_0_0 = blocks.keep_m_in_n(gr.sizeof_gr_complex, vr2fft, fftm, vr2offset)
-        self.blocks_keep_m_in_n_0 = blocks.keep_m_in_n(gr.sizeof_gr_complex, vr1fft, fftm, vr1offset)
-        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, int(10e6))
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vcc((amplitude2, ))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((amplitude1, ))
+        self.blocks_head_0_0 = blocks.head(gr.sizeof_gr_complex*1, int(50e6))
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, int(50e6))
         self.blocks_file_sink_0_0_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, filepattern + "-pos-vr1.bin", False)
         self.blocks_file_sink_0_0_0_0.set_unbuffered(False)
         self.blocks_file_sink_0_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, filepattern + "-pos-vr2.bin", False)
@@ -295,35 +246,19 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
         self.connect((self.analog_random_source_x_0_1, 0), (self.blocks_stream_to_tagged_stream_0_1, 0))
         self.connect((self.blocks_head_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_head_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_keep_m_in_n_0, 0), (self.blocks_stream_to_vector_0_0, 0))
-        self.connect((self.blocks_keep_m_in_n_0_0, 0), (self.blocks_stream_to_vector_0_0_0, 0))
+        self.connect((self.blocks_head_0_0, 0), (self.blocks_file_sink_0_0, 0))
+        self.connect((self.blocks_head_0_0, 0), (self.blocks_file_sink_0_0_0, 0))
+        self.connect((self.blocks_head_0_0, 0), (self.qtgui_freq_sink_x_0_0_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.hydra_hydra_sink_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.hydra_hydra_sink_0, 1))
-        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.blocks_file_sink_0_0_0_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.qtgui_freq_sink_x_0_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_1_0, 0), (self.blocks_file_sink_0_0_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_1_0, 0), (self.qtgui_freq_sink_x_0_0_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.blocks_throttle_1, 0))
         self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_ofdm_tx_0, 0))
         self.connect((self.blocks_stream_to_tagged_stream_0_1, 0), (self.digital_ofdm_tx_0_1, 0))
-        self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.blocks_stream_to_vector_0_0, 0), (self.fft_vxx_0_0, 0))
-        self.connect((self.blocks_stream_to_vector_0_0_0, 0), (self.fft_vxx_0_0_0, 0))
-        self.connect((self.blocks_stream_to_vector_0_1, 0), (self.fft_vxx_0_1, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_to_vector_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_to_vector_0_1, 0))
-        self.connect((self.blocks_vector_to_stream_0, 0), (self.blocks_keep_m_in_n_0, 0))
-        self.connect((self.blocks_vector_to_stream_0_0, 0), (self.blocks_multiply_const_vxx_1, 0))
-        self.connect((self.blocks_vector_to_stream_0_0_0, 0), (self.blocks_multiply_const_vxx_1_0, 0))
-        self.connect((self.blocks_vector_to_stream_0_1, 0), (self.blocks_keep_m_in_n_0_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_file_sink_0_0_0_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0_0, 0))
+        self.connect((self.blocks_throttle_1, 0), (self.blocks_head_0_0, 0))
         self.connect((self.digital_ofdm_tx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.digital_ofdm_tx_0_1, 0), (self.blocks_multiply_const_vxx_0_0, 0))
-        self.connect((self.fft_vxx_0, 0), (self.blocks_vector_to_stream_0, 0))
-        self.connect((self.fft_vxx_0_0, 0), (self.blocks_vector_to_stream_0_0, 0))
-        self.connect((self.fft_vxx_0_0_0, 0), (self.blocks_vector_to_stream_0_0_0, 0))
-        self.connect((self.fft_vxx_0_1, 0), (self.blocks_vector_to_stream_0_1, 0))
         self.connect((self.hydra_hydra_sink_0, 0), (self.blocks_head_0, 0))
 
     def closeEvent(self, event):
@@ -336,8 +271,6 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
 
     def set_fftm(self, fftm):
         self.fftm = fftm
-        self.blocks_keep_m_in_n_0_0.set_n(self.fftm)
-        self.blocks_keep_m_in_n_0.set_n(self.fftm)
 
     def get_filepattern(self):
         return self.filepattern
@@ -354,20 +287,19 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
 
     def set_freq(self, freq):
         self.freq = freq
+        self.hydra_hydra_sink_0.set_central_frequency(0, self.freq)
 
     def get_freq1(self):
         return self.freq1
 
     def set_freq1(self, freq1):
         self.freq1 = freq1
-        self.hydra_hydra_sink_0.set_central_frequency(0, self.freq1)
 
     def get_freq2(self):
         return self.freq2
 
     def set_freq2(self, freq2):
         self.freq2 = freq2
-        self.hydra_hydra_sink_0.set_central_frequency(1, self.freq2)
 
     def get_samprate(self):
         return self.samprate
@@ -392,30 +324,24 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
 
     def set_vr1fft(self, vr1fft):
         self.vr1fft = vr1fft
-        self.blocks_multiply_const_vxx_1.set_k((1.0/self.vr1fft, ))
-        self.blocks_keep_m_in_n_0.set_m(self.vr1fft)
 
     def get_vr1offset(self):
         return self.vr1offset
 
     def set_vr1offset(self, vr1offset):
         self.vr1offset = vr1offset
-        self.blocks_keep_m_in_n_0.set_offset(self.vr1offset)
 
     def get_vr2fft(self):
         return self.vr2fft
 
     def set_vr2fft(self, vr2fft):
         self.vr2fft = vr2fft
-        self.blocks_multiply_const_vxx_1_0.set_k((1.0/self.vr2fft, ))
-        self.blocks_keep_m_in_n_0_0.set_m(self.vr2fft)
 
     def get_vr2offset(self):
         return self.vr2offset
 
     def set_vr2offset(self, vr2offset):
         self.vr2offset = vr2offset
-        self.blocks_keep_m_in_n_0_0.set_offset(self.vr2offset)
 
     def get_pilot_carriers(self):
         return self.pilot_carriers
@@ -486,27 +412,41 @@ class hydra_dump_to_files(gr.top_block, Qt.QWidget):
     def set_length_tag_key(self, length_tag_key):
         self.length_tag_key = length_tag_key
 
+    def get_amplitude2(self):
+        return self.amplitude2
+
+    def set_amplitude2(self, amplitude2):
+        self.amplitude2 = amplitude2
+        self.blocks_multiply_const_vxx_0_0.set_k((self.amplitude2, ))
+
+    def get_amplitude1(self):
+        return self.amplitude1
+
+    def set_amplitude1(self, amplitude1):
+        self.amplitude1 = amplitude1
+        self.blocks_multiply_const_vxx_0.set_k((self.amplitude1, ))
+
 
 def argument_parser():
     description = 'Example of an OFDM Transmitter'
     parser = OptionParser(usage="%prog: [options]", option_class=eng_option, description=description)
     parser.add_option(
-        "", "--fftm", dest="fftm", type="intx", default=512,
+        "", "--fftm", dest="fftm", type="intx", default=128,
         help="Set fftm [default=%default]")
     parser.add_option(
         "", "--filepattern", dest="filepattern", type="string", default="./",
         help="Set ./ [default=%default]")
     parser.add_option(
-        "", "--vr1fft", dest="vr1fft", type="intx", default=170,
+        "", "--vr1fft", dest="vr1fft", type="intx", default=512,
         help="Set vr1fft [default=%default]")
     parser.add_option(
         "", "--vr1offset", dest="vr1offset", type="intx", default=0,
         help="Set vr1offset [default=%default]")
     parser.add_option(
-        "", "--vr2fft", dest="vr2fft", type="intx", default=85,
+        "", "--vr2fft", dest="vr2fft", type="intx", default=512,
         help="Set vr2fft [default=%default]")
     parser.add_option(
-        "", "--vr2offset", dest="vr2offset", type="intx", default=0,
+        "", "--vr2offset", dest="vr2offset", type="intx", default=319,
         help="Set vr2offset [default=%default]")
     return parser
 
