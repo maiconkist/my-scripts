8fcdee14d3e53bf54db8d36eebb5e8a7fa3a854f
Maicon Kist <maiconkist@gmail.com>
2019-02-27 13:42:31 +0000

    addef fec to lora side1 1

--- 19_comnet_iot/grc/lora_side1.py
+++ 19_comnet_iot/grc/lora_side1.py
@@ -3,59 +3,115 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Lora Side1
-# Generated: Mon Feb 25 16:11:43 2019
+# Generated: Wed Feb 27 13:41:54 2019
 ##################################################
 
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
 from gnuradio import blocks
+from gnuradio import digital
 from gnuradio import eng_notation
+from gnuradio import fec
 from gnuradio import gr
 from gnuradio import uhd
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from gnuradio.filter import pfb
+from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
 import lora
 import math
+import sys
 import time
+from gnuradio import qtgui
 
 
-class lora_side1(gr.top_block):
+class lora_side1(gr.top_block, Qt.QWidget):
 
-    def __init__(self, bw=100e3, frequency_rx=2.4505e9 + 5e6, frequency_tx=2.4505e9, gain=0.9, offset=0):
+    def __init__(self, MTU=1000, bw=100e3, frequency_rx=2.4505e9 + 3e6, frequency_tx=2.4505e9, offset=0):
         gr.top_block.__init__(self, "Lora Side1")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Lora Side1")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "lora_side1")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
         ##################################################
         # Parameters
         ##################################################
+        self.MTU = MTU
         self.bw = bw
         self.frequency_rx = frequency_rx
         self.frequency_tx = frequency_tx
-        self.gain = gain
         self.offset = offset
 
         ##################################################
         # Variables
         ##################################################
+        self.rep = rep = 3
         self.spreading_factor = spreading_factor = 8
         self.samp_rate = samp_rate = 500e3
         self.ldr = ldr = True
         self.header = header = False
+        self.gain_tx = gain_tx = 0.8
+        self.gain_rx = gain_rx = 0.8
+
+
+        self.enc_rep = enc_rep = fec.repetition_encoder_make(MTU*8, rep)
+
+
+
+        self.enc_dummy = enc_dummy = fec.dummy_encoder_make(MTU*8)
+
+
+
+        self.enc_ccsds = enc_ccsds = fec.ccsds_encoder_make(MTU*8, 0, fec.CC_TERMINATED)
+
+
+
+        self.dec_rep = dec_rep = fec.repetition_decoder.make(MTU*8, rep, 0.5)
+
+
+
+        self.dec_dummy = dec_dummy = fec.dummy_decoder.make(MTU*8)
+
+
+
+        self.dec_cc = dec_cc = fec.cc_decoder.make(8000, 7, 2, ([109,79]), 0, -1, fec.CC_TERMINATED, False)
+
         self.code_rate = code_rate = 4
 
         ##################################################
         # Blocks
         ##################################################
-        self.uhd_usrp_source_0 = uhd.usrp_source(
-        	",".join(('', '')),
-        	uhd.stream_args(
-        		cpu_format="fc32",
-        		channels=range(1),
-        	),
-        )
-        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_source_0.set_center_freq(frequency_rx, 0)
-        self.uhd_usrp_source_0.set_gain(0, 0)
-        self.uhd_usrp_source_0.set_antenna("RX2", 0)
+        self._gain_tx_range = Range(0, 1, 0.01, 0.8, 200)
+        self._gain_tx_win = RangeWidget(self._gain_tx_range, self.set_gain_tx, 'gain_tx', "counter_slider", float)
+        self.top_layout.addWidget(self._gain_tx_win)
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
         	",".join(("", "")),
         	uhd.stream_args(
@@ -65,7 +121,7 @@ class lora_side1(gr.top_block):
         )
         self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
         self.uhd_usrp_sink_0.set_center_freq(frequency_tx, 0)
-        self.uhd_usrp_sink_0.set_gain(gain, 0)
+        self.uhd_usrp_sink_0.set_normalized_gain(gain_tx, 0)
         self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
         self.pfb_arb_resampler_xxx_0_0 = pfb.arb_resampler_ccf(
         	  bw/samp_rate,
@@ -83,25 +139,49 @@ class lora_side1(gr.top_block):
         self.lora_encode_0 = lora.encode(spreading_factor, code_rate, ldr, header)
         self.lora_demod_0 = lora.demod(spreading_factor, ldr, 25.0, 2)
         self.lora_decode_0 = lora.decode(spreading_factor, code_rate, ldr, header)
-        self.blocks_tuntap_pdu_0 = blocks.tuntap_pdu('tap0', 10000, False)
+        self._gain_rx_range = Range(0, 1, 0.01, 0.8, 200)
+        self._gain_rx_win = RangeWidget(self._gain_rx_range, self.set_gain_rx, 'gain_rx', "counter_slider", float)
+        self.top_layout.addWidget(self._gain_rx_win)
+        self.fec_async_encoder_0 = fec.async_encoder(enc_ccsds, False, True, True, MTU)
+        self.fec_async_decoder_0 = fec.async_decoder(dec_cc, False, True, MTU)
+        self.digital_map_bb_0 = digital.map_bb(([-1,1]))
+        self.blocks_tuntap_pdu_0 = blocks.tuntap_pdu('tap0', MTU, False)
+        self.blocks_tagged_stream_to_pdu_1 = blocks.tagged_stream_to_pdu(blocks.byte_t, "len")
         self.blocks_rotator_cc_0_0 = blocks.rotator_cc((2 * math.pi * offset) / samp_rate)
         self.blocks_rotator_cc_0 = blocks.rotator_cc((2 * math.pi * offset) / samp_rate)
+        self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, "len" )
+        self.blocks_null_source_0 = blocks.null_source(gr.sizeof_gr_complex*1)
         self.blocks_message_debug_0 = blocks.message_debug()
 
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.blocks_tuntap_pdu_0, 'pdus'), (self.lora_encode_0, 'in'))
-        self.msg_connect((self.lora_decode_0, 'out'), (self.blocks_message_debug_0, 'print'))
-        self.msg_connect((self.lora_decode_0, 'out'), (self.blocks_tuntap_pdu_0, 'pdus'))
+        self.msg_connect((self.blocks_tagged_stream_to_pdu_1, 'pdus'), (self.fec_async_decoder_0, 'in'))
+        self.msg_connect((self.blocks_tuntap_pdu_0, 'pdus'), (self.fec_async_encoder_0, 'in'))
+        self.msg_connect((self.fec_async_decoder_0, 'out'), (self.blocks_tuntap_pdu_0, 'pdus'))
+        self.msg_connect((self.fec_async_encoder_0, 'out'), (self.lora_encode_0, 'in'))
+        self.msg_connect((self.lora_decode_0, 'out'), (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
         self.msg_connect((self.lora_demod_0, 'out'), (self.lora_decode_0, 'in'))
         self.msg_connect((self.lora_encode_0, 'out'), (self.lora_mod_0, 'in'))
+        self.connect((self.blocks_null_source_0, 0), (self.blocks_rotator_cc_0_0, 0))
+        self.connect((self.blocks_pdu_to_tagged_stream_0, 0), (self.digital_map_bb_0, 0))
         self.connect((self.blocks_rotator_cc_0, 0), (self.pfb_arb_resampler_xxx_0, 0))
         self.connect((self.blocks_rotator_cc_0_0, 0), (self.pfb_arb_resampler_xxx_0_0, 0))
+        self.connect((self.digital_map_bb_0, 0), (self.blocks_tagged_stream_to_pdu_1, 0))
         self.connect((self.lora_mod_0, 0), (self.blocks_rotator_cc_0, 0))
         self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.uhd_usrp_sink_0, 0))
         self.connect((self.pfb_arb_resampler_xxx_0_0, 0), (self.lora_demod_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_rotator_cc_0_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "lora_side1")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_MTU(self):
+        return self.MTU
+
+    def set_MTU(self, MTU):
+        self.MTU = MTU
 
     def get_bw(self):
         return self.bw
@@ -116,7 +196,6 @@ class lora_side1(gr.top_block):
 
     def set_frequency_rx(self, frequency_rx):
         self.frequency_rx = frequency_rx
-        self.uhd_usrp_source_0.set_center_freq(self.frequency_rx, 0)
 
     def get_frequency_tx(self):
         return self.frequency_tx
@@ -125,14 +204,6 @@ class lora_side1(gr.top_block):
         self.frequency_tx = frequency_tx
         self.uhd_usrp_sink_0.set_center_freq(self.frequency_tx, 0)
 
-    def get_gain(self):
-        return self.gain
-
-    def set_gain(self, gain):
-        self.gain = gain
-        self.uhd_usrp_sink_0.set_gain(self.gain, 0)
-
-
     def get_offset(self):
         return self.offset
 
@@ -140,6 +211,12 @@ class lora_side1(gr.top_block):
         self.offset = offset
         self.blocks_rotator_cc_0_0.set_phase_inc((2 * math.pi * self.offset) / self.samp_rate)
         self.blocks_rotator_cc_0.set_phase_inc((2 * math.pi * self.offset) / self.samp_rate)
+
+    def get_rep(self):
+        return self.rep
+
+    def set_rep(self, rep):
+        self.rep = rep
 
     def get_spreading_factor(self):
         return self.spreading_factor
@@ -152,7 +229,6 @@ class lora_side1(gr.top_block):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
         self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
         self.pfb_arb_resampler_xxx_0_0.set_rate(self.bw/self.samp_rate)
         self.pfb_arb_resampler_xxx_0.set_rate(self.samp_rate/self.bw)
@@ -171,6 +247,56 @@ class lora_side1(gr.top_block):
     def set_header(self, header):
         self.header = header
 
+    def get_gain_tx(self):
+        return self.gain_tx
+
+    def set_gain_tx(self, gain_tx):
+        self.gain_tx = gain_tx
+        self.uhd_usrp_sink_0.set_normalized_gain(self.gain_tx, 0)
+
+
+    def get_gain_rx(self):
+        return self.gain_rx
+
+    def set_gain_rx(self, gain_rx):
+        self.gain_rx = gain_rx
+
+    def get_enc_rep(self):
+        return self.enc_rep
+
+    def set_enc_rep(self, enc_rep):
+        self.enc_rep = enc_rep
+
+    def get_enc_dummy(self):
+        return self.enc_dummy
+
+    def set_enc_dummy(self, enc_dummy):
+        self.enc_dummy = enc_dummy
+
+    def get_enc_ccsds(self):
+        return self.enc_ccsds
+
+    def set_enc_ccsds(self, enc_ccsds):
+        self.enc_ccsds = enc_ccsds
+
+    def get_dec_rep(self):
+        return self.dec_rep
+
+    def set_dec_rep(self, dec_rep):
+        self.dec_rep = dec_rep
+
+    def get_dec_dummy(self):
+        return self.dec_dummy
+
+    def set_dec_dummy(self, dec_dummy):
+        self.dec_dummy = dec_dummy
+
+    def get_dec_cc(self):
+        return self.dec_cc
+
+    def set_dec_cc(self, dec_cc):
+        self.dec_cc = dec_cc
+
     def get_code_rate(self):
         return self.code_rate
 
@@ -181,14 +307,14 @@ class lora_side1(gr.top_block):
 def argument_parser():
     parser = OptionParser(usage="%prog: [options]", option_class=eng_option)
     parser.add_option(
-        "", "--frequency-rx", dest="frequency_rx", type="eng_float", default=eng_notation.num_to_str(2.4505e9 + 5e6),
+        "", "--MTU", dest="MTU", type="intx", default=1000,
+        help="Set MTU [default=%default]")
+    parser.add_option(
+        "", "--frequency-rx", dest="frequency_rx", type="eng_float", default=eng_notation.num_to_str(2.4505e9 + 3e6),
         help="Set frequency_rx [default=%default]")
     parser.add_option(
         "", "--frequency-tx", dest="frequency_tx", type="eng_float", default=eng_notation.num_to_str(2.4505e9),
         help="Set frequency_tx [default=%default]")
-    parser.add_option(
-        "", "--gain", dest="gain", type="eng_float", default=eng_notation.num_to_str(0.9),
-        help="Set gain [default=%default]")
     return parser
 
 
@@ -196,9 +322,21 @@ def main(top_block_cls=lora_side1, options=None):
     if options is None:
         options, _ = argument_parser().parse_args()
 
-    tb = top_block_cls(frequency_rx=options.frequency_rx, frequency_tx=options.frequency_tx, gain=options.gain)
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls(MTU=options.MTU, frequency_rx=options.frequency_rx, frequency_tx=options.frequency_tx)
     tb.start()
-    tb.wait()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
 
 
 if __name__ == '__main__':
